f88ca80d4b120d4689923c0e2475e4f5f337acec
bmckalip <brmckalip@gmail.com>
2017-01-19 02:36:57 -0500

    working solver

--- main.py
+++ main.py
@@ -1,89 +1,110 @@
 import sys
+import copy
 
-goalState = [[1, 2, 3],
-             [8, 0, 4],
-             [7, 6, 5]
-             ]
+class board:
+    def __init__(self, start, goal):
+        self.start = start
+        self.goal = goal
+        self.cost = 0
+        self.path = []
 
-# locates the index of a tile on a given board
-def locateTile(board, tile):
-    for index, row in enumerate(board):
-        if tile in row:
-            return [index, row.index(tile)]
+    # locates the index of a tile on a given board
+    @staticmethod
+    def locateTile(board, tile):
+        for index, row in enumerate(board):
+            if tile in row:
+                return [index, row.index(tile)]
 
-# calculates manhattan distance heuristic
-def manhattanDistance(start, goal, tile):
-    # find indices of tile in both start and goal
-    startLoc = locateTile(start, tile)
-    goalLoc = locateTile(goal, tile)
+    # calculates manhattan distance heuristic
+    def manhattanDistance(self, tile):
+        # find indices of tile in both start and goal
+        startLoc = self.locateTile(self.start, tile)
+        goalLoc = self.locateTile(self.goal, tile)
 
-    # subtract the indices to calculate the distance and return it
-    return abs(startLoc[0] - goalLoc[0]) + abs(startLoc[1] - goalLoc[1])
+        # subtract the indices to calculate the distance and return it
+        return abs(startLoc[0] - goalLoc[0]) + abs(startLoc[1] - goalLoc[1])
 
-def getAdjacentTiles(start, tile):
-    baseTileLoc = locateTile(start, tile)
-    results = []
+    # returns all adjacent tiles to the specified tile
+    def getAdjacentTiles(self, tile):
+        baseTileLoc = self.locateTile(self.start, tile)
+        results = []
 
-    adjTiles = [[baseTileLoc[0] + 1, baseTileLoc[1]],
-                [baseTileLoc[0], baseTileLoc[1] + 1],
-                [baseTileLoc[0] - 1, baseTileLoc[1]],
-                [baseTileLoc[0], baseTileLoc[1] - 1]
-                ]
+        adjTiles = [[baseTileLoc[0] + 1, baseTileLoc[1]],
+                    [baseTileLoc[0], baseTileLoc[1] + 1],
+                    [baseTileLoc[0] - 1, baseTileLoc[1]],
+                    [baseTileLoc[0], baseTileLoc[1] - 1]
+                    ]
 
-    for adjTile in adjTiles:
-        if all(i > 0 and i < 3 for i in adjTile):
-            results.append(start[adjTile[0]][adjTile[1]])
+        # bound checking
+        for adjTile in adjTiles:
+            if all(i >= 0 and i <= 2 for i in adjTile):
+                results.append(start[adjTile[0]][adjTile[1]])
 
-    return results
+        return results
+
+    # returns the optimal move based on the lowest overall stochasticity of a proposed move
+    def getBestMove(self, moves):
+        bestMove = None
+        bestSum = sys.maxsize
+        for move in moves:
+            sum = 0
+            testBoard = copy.deepcopy(self)
+            testBoard.moveTile(move)
+            for tile in range(1, 9):
+                sum += testBoard.manhattanDistance(tile)
+
+            if sum < bestSum:
+                bestSum = sum
+                bestMove = move
+
+        self.cost += bestMove
+        return bestMove
+
+    # swaps the positions of the specified tile and the empty tile
+    def moveTile(self, tile):
+        emptyTile = self.locateTile(self.start, 0)
+        tileToMove = self.locateTile(self.start, tile)
+
+        self.start[emptyTile[0]][emptyTile[1]] = tile
+        self.start[tileToMove[0]][tileToMove[1]] = 0
+        self.path.append(tile)
 
 
-def moveTile(start, tile):
-    emptyTile = locateTile(start, 0)
-    tileToMove = locateTile(start, tile)
+# read input state from file and save it as a 2D (3x3) list, create the goal state
+start = []
+goal = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
 
-    newBoard = start
-    newBoard[emptyTile[0]][emptyTile[1]] = tile
-    newBoard[tileToMove[0]][tileToMove[1]] = 0
-    return newBoard
-
-# read input state from file and save it as a 2D (3x3) list
 f = open("startState", 'r')
-startState = []
 for line in f.readlines():
-    startState.append(list(map(int, line.strip())))
+    start.append(list(map(int, line.strip())))
 
-#print(goalState[0], goalState[1], goalState[2])
-print('', startState[0], '\n', startState[1], '\n', startState[2], '\n')
-tile = 1
-# f = h + g
+print('', start[0], '\n', start[1], '\n', start[2])
 
-pathCost = 0
+currentState = board(start, goal)
 prevMove = 0
 bestMove = 0
-while startState != goalState:
+
+# main algorithm loop
+while currentState.start != goal:
     # find all tiles adjacent to the empty tile
-    availableMoves = getAdjacentTiles(startState, 0)
+    moves = currentState.getAdjacentTiles(0)
 
     # disallow moving the same tile twice in a row (back and forth endlessly)
-    if prevMove in availableMoves:
-        availableMoves.remove(prevMove)
+    while True:
+        bestMove = currentState.getBestMove(moves)
+        if bestMove != prevMove:
+            prevMove = bestMove
+            break
+        else:
+            moves.remove(bestMove)
 
-    # find the lowest cost tile to move which is adjacent to the empty tile
-    lowestCost = sys.maxsize
-    for tile in availableMoves:
-        h = manhattanDistance(startState, goalState, tile)
-        # f = g(tile) + h(manhatdist)
-        if tile + h < lowestCost:
-            lowestCost = tile + h
-            bestMove = tile
-    # execute the move
-    startState = moveTile(startState, bestMove)
+    # execute the move. uncomment print() statement to print the board after every step
+    currentState.moveTile(bestMove)
+    # print('\n', currentState.start[0], '\n', currentState.start[1], '\n', currentState.start[2])
 
-    # add the move to the overall cost of the path
-    pathCost += lowestCost
-    prevMove = bestMove
-    bestMove = 0
-    print('', startState[0], '\n', startState[1], '\n', startState[2], '\n', 'cost:', lowestCost, 'total Cost:', pathCost)
+# print Path information
+print("\n Path: " + str(currentState.path),
+      "\n Path Cost: " + str(currentState.cost)
+      )
 
-print(pathCost)
 
